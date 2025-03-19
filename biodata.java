2bf4dc824d130cc53e0533a985083d2bffcb1a1f
Pankaj9612 <163497297+Pankaj9612@users.noreply.github.com>
2025-03-19 13:59:41 +0530

    Add files via upload

---
+++ biodata.java
@@ -0,0 +1,17 @@
+class biodata
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Name:-Pankaj Govardhan Gaikwad");
+		System.out.println("City:-Dharashiv");
+		System.out.println("Village:-Chilwadi");
+		System.out.println("Age:-22");
+		System.out.println("Gender:-Male");
+		System.out.println("Place Of Birth:-Solapur");
+		System.out.println("Date Of Birth:-27-09-2003");
+		System.out.println("Caste:Maratha");
+		System.out.println("Highest Education:-BTech");
+		System.out.println("Contact Number:-9090909090");
+		System.out.println("Email ID:-pankaj@gmail.com");
+	}
+}
